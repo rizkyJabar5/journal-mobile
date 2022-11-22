c807d6a89c61decde8b709eb12f935dc2fbe9ce2
rizkyJabar5 <rkhamet12@gmail.com>
2022-11-22 18:42:04 +0700

    Initial mobile apps

---
+++ android/app/src/main/java/com/florist/journal_florist/MainActivity.java
@@ -0,0 +1,6 @@
+package com.florist.journal_florist;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class Bottombar extends StatefulWidget {
+  Bottombar({Key? key}) : super(key: key);
+
+  @override
+  _BottombarState createState() => _BottombarState();
+}
+
+class _BottombarState extends State<Bottombar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
