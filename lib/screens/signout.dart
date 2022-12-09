d2f2c698ec7bf427116894e0bba670c22a6bdc26
rizkyJabar5 <rkhamet12@gmail.com>
2022-12-09 11:47:33 +0700

    Add signout

---
+++ lib/screens/signout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Signout extends StatefulWidget {
+  Signout({Key? key}) : super(key: key);
+
+  @override
+  _SignoutState createState() => _SignoutState();
+}
+
+class _SignoutState extends State<Signout> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (() => {}),
+    );
+  }
+}
