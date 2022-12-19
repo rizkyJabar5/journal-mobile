c5e6acc8475e7bdbf90e086e519a95863bacd4a0
rizky Jabar <rkhamet12@gmail.com>
2022-12-19 19:28:07 +0700

    - Changes splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,6 +5,8 @@ import 'package:journal_florist/features/networking/token_repository.dart';
 import 'package:journal_florist/screens/login.dart';
 import 'package:journal_florist/widgets/bottom_bar.dart';
 
+import '../utilities/app_styles.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -48,10 +50,45 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Image.asset(
-          "assets/images/logo.jpg",
-          width: 500,
-          height: 500,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/logo.jpg",
+              width: 500,
+              height: 500,
+            ),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                  text: "Welcome\n",
+                  style: TextStyle(
+                      color: Styles.primaryAccent,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Poppins"),
+                  children: [
+                    TextSpan(
+                      text: 'To the\n',
+                      style: TextStyle(
+                        color: Colors.deepPurple[200],
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: "Poppins",),
+                    ),
+                    TextSpan(
+                      text: 'Journal Florist',
+                      style: TextStyle(
+                        color: Styles.primaryAccent,
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Pacifico",),
+                    ),
+                  ],
+              ),
+            ),
+          ],
         ),
       ),
     );
